dd36b435cd7accce87bb0ab86dfc3b48fc40a2f8
Chrisleerbeer <chris.v.d.lee2004@gmail.com>
2024-09-03 13:06:55 +0200

    Initial commit

---
+++ Assets/Code/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float _speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, vertical, 0);
+
+        transform.Translate(direction * _speed * Time.deltaTime);
+
+    }
+}
