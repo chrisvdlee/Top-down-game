99ab9898b232a44875a7c03ef988ffc60ac7b2f2
chrisvdlee <chris.v.d.lee2004@gmail.com>
2024-09-30 20:33:51 +0200

    Improve playermovement

--- Assets/Code/Scripts/PlayerController.cs
+++ Assets/Code/Scripts/PlayerController.cs
@@ -6,9 +6,11 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float movementSpeed = 5f;  // Movement speed
     [SerializeField] private float dodgeForce = 20f;
     [SerializeField] private float dodgeDuration = 0.2f;  // How long the dodge lasts
+    [SerializeField] private float dodgeCooldown = 1f;  // How long until you can use the dodge again
 
     private Rigidbody2D rb;  // Reference to Rigidbody2D
     private bool isDodging = false;
+    private bool canDodge = true;
 
     void Start()
     {
@@ -31,7 +33,7 @@ public class PlayerController : MonoBehaviour
     private void HandleInput()
     {
         // Check for dodge input
-        if (Input.GetButtonDown("Jump") && !isDodging)
+        if (Input.GetButtonDown("Jump") && !isDodging && canDodge)
         {
             StartDodge();
         }
@@ -40,10 +42,10 @@ public class PlayerController : MonoBehaviour
     private void MovePlayer()
     {
         // Capture movement input
-        Vector2 moveDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+        Vector2 moveDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
 
         // Set velocity based on movement direction
-        rb.velocity = moveDirection * (moveDirection != Vector2.zero ? movementSpeed : 0);
+        rb.velocity = moveDirection * movementSpeed; //(moveDirection != Vector2.zero ? movementSpeed : 0);
     }
 
     private void StartDodge()
@@ -62,8 +64,11 @@ public class PlayerController : MonoBehaviour
     private IEnumerator Dodge(Vector2 dodgeDirection)
     {
         isDodging = true;
+        canDodge = false;
         rb.velocity = dodgeDirection * dodgeForce;  // Apply dodge force
         yield return new WaitForSeconds(dodgeDuration);  // Wait for dodge duration
         isDodging = false;  // Reset dodge
+        yield return new WaitForSeconds(dodgeCooldown);  // Wait for dodge cooldown
+        canDodge = true;
     }
 }
